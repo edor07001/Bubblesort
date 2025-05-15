3c78860d1c5c98ac57926cdb0d0d0ef7fdbb8d09
edor07001 <edor07001@utb.vaxjo.se>
2025-05-15 22:08:14 +0200

    Bubblesort

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+
+const int size = 100000;
+int[] numbers = new int[size];
+Random rng = new Random();
+
+for(int i = 0; i < size; i++)
+{
+    numbers[i] = rng.Next();
+}
+
+Stopwatch sw = Stopwatch.StartNew();
+
+BubbleSort(numbers);
+
+sw.Stop();
+Console.WriteLine($"Tid för sortering: {sw.Elapsed}");
+
+for(int i = 0; i < size; i++)
+{
+    Console.WriteLine(numbers[i]);
+}
+
+static void BubbleSort(int[] arr)
+{
+    int n = arr.Length;
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
